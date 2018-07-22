da416a5afc31a4b4d6dfdaec26f8e1bbb7c236fe
Valentin Raduti <valentin@thedotin.ro>
2018-07-22 14:20:28 +0300

    working symfony rest

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mlnr-php
+
+Get it running:
+
+```
+cd server
+composer install
+php bin/console server:run
+```
---
+++ server/src/Entity/User.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Table(name="`user`")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer", name="`id`",)
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", name="`email`", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string",name="`password`", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", name="`name`", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="boolean", name="`active`",)
+     */
+    private $active;
+
+    /**
+     * @ORM\Column(type="string", name="`role`", length=16)
+     */
+    private $role;
+
+    /**
+     * @ORM\Column(type="string", name="`rank`", length=16)
+     */
+    private $rank;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getActive(): ?bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getRank(): ?string
+    {
+        return $this->rank;
+    }
+
+    public function setRank(string $rank): self
+    {
+        $this->rank = $rank;
+
+        return $this;
+    }
+}
