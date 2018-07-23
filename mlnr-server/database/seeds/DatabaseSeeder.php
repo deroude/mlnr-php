4a2cd14b8a4b3b88ad2b5bf5d57dc70e695cfd0e
Valentin Raduti <valentin@thedotin.ro>
2018-07-23 19:17:45 +0300

    laravel

---
+++ mlnr-server/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+        factory(App\User::class, 10)->create();
+        Model::reguard();
+    }
+}
